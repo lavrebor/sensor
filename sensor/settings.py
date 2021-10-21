571748d1387255fbb46376e5ff0e86f7cf4b551c
lavrebor <teclado7704**>
2021-10-21 16:42:18 -0300

    Eliminação de linhas da configuração

--- sensor/settings.py
+++ sensor/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'sensor.wsgi.application'
 DATABASES = {
 
     'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbsensor',
-
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
